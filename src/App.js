4900ae30f0c2c9ff7ff1e0319969b6944219941e
prantokhaan <prantokhaan@gmail.com>
2021-09-26 03:21:34 +0600

    data added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const musicBand = [
-    {
-      name: 'Ashes',
-      vocal: 'Zunayed Evan',
-      guitarist: 'Sultan Rafsan',
-      genre: '',
-      age: '',
-      fee: '',
-      popular_song: '',
-      cover_photo: ''
-    },
-    
-  ]
+  
   return (
     <div>
       <h2>This is Image Section</h2>
